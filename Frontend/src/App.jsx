6f17831815e9e669fff033dbc0412d6efdc76572
Rajdeep Mandal <mrajdeep651@gmail.com>
2025-03-12 17:27:41 +0530

    Updated from

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,16 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 import Dashboard from "./Pages/Dashboard";
-import CarbonCreditDashboard from "./components/CarbonCreditDashboard"; // Fixed import
+import CarbonCreditDashboard from "./components/CarbonCreditDashboard";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/carbon-credits" element={<CarbonCreditDashboard />} /> {/* Fixed Component Name */}
+        <Route path="/carbon-credits" element={<CarbonCreditDashboard />} />
       </Routes>
     </Router>
   );
--- Frontend/src/Pages/Signup.jsx
+++ Frontend/src/Pages/Signup.jsx
@@ -1,76 +1,184 @@
-import React from "react";
-import backgroundImage from "../assets/image1.png";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import backgroundImage from "../assets/image1.png";
 
 const Signup = () => {
   const navigate = useNavigate();
-
-  const handleSignup = () => {
-    navigate("/dashboard");
-  };
+  const [businessSector, setBusinessSector] = useState("");
+  const [otherSector, setOtherSector] = useState("");
 
   return (
     <div
-      className="min-h-screen bg-[#fcfcfc] brightness-90 bg-bottom bg-no-repeat"
-      style={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: "100vw 70vh",
-      }}
+      className="min-h-screen flex justify-center items-center bg-cover bg-center bg-no-repeat p-4"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <div className="flex justify-center items-start min-h-screen pt-20">
-        <div className="p-8 rounded-xl w-full max-w-md bg-opacity-80 backdrop-blur-lg shadow-lg">
-          <h2 className="text-2xl font-extrabold text-center mb-10 text-gray-800 tracking-wider">
-            SIGN UP
-          </h2>
+      <div className="w-full max-w-4xl bg-white/60 backdrop-blur-lg rounded-xl shadow-lg p-6 md:p-8">
+        <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
+          Create an Account
+        </h2>
 
-          <form className="space-y-4">
-            <input
-              type="text"
-              placeholder="Full Name"
-              className="w-full px-4 py-3 border border-black rounded-xl focus:ring focus:ring-green-300"
-            />
-            <input
-              type="text"
-              placeholder="Aadhar Card Number"
-              className="w-full px-4 py-3 border border-black rounded-xl focus:ring focus:ring-green-300"
-            />
-            <input
-              type="text"
-              placeholder="Mobile Number"
-              className="w-full px-4 py-3 border border-black rounded-xl focus:ring focus:ring-green-300"
-            />
-            <input
-              type="text"
-              placeholder="Company Name"
-              className="w-full px-4 py-3 border border-black rounded-xl focus:ring focus:ring-green-300"
-            />
-            <input
-              type="text"
-              placeholder="GSTIN Number"
-              className="w-full px-4 py-3 border border-black rounded-xl focus:ring focus:ring-green-300"
-            />
-            <select className="w-full px-4 py-3 border border-black rounded-xl focus:ring focus:ring-green-300">
-              <option value="seller">Seller Account</option>
-              <option value="buyer">Buyer Account</option>
-            </select>
-            <div className=" flex justify-center items-center">
-              <button
-                onClick={handleSignup}
-                className="w-1/2 bg-green-600 text-white py-3 font-bold rounded-lg hover:bg-green-700 transition"
-              >
-                Sign Up
-              </button>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {/* left */}
+          <div>
+            <h3 className="text-xl font-semibold text-gray-700 mb-3">
+              Personal Details
+            </h3>
+            <div className="space-y-4">
+              <div>
+                <label className="block text-gray-700">Full Name</label>
+                <input
+                  type="text"
+                  className="w-full p-2 border rounded focus:ring focus:ring-indigo-300"
+                  placeholder="John Doe"
+                />
+              </div>
+              <div>
+                <label className="block text-gray-700">Email</label>
+                <input
+                  type="email"
+                  className="w-full p-2 border rounded focus:ring focus:ring-indigo-300"
+                  placeholder="email@example.com"
+                />
+              </div>
+              <div>
+                <label className="block text-gray-700">Phone Number</label>
+                <input
+                  type="text"
+                  className="w-full p-2 border rounded focus:ring focus:ring-indigo-300"
+                  placeholder="+1234567890"
+                />
+              </div>
+              <div>
+                <label className="block text-gray-700">Password</label>
+                <input
+                  type="password"
+                  className="w-full p-2 border rounded focus:ring focus:ring-indigo-300"
+                  placeholder="Set Password"
+                />
+              </div>
+              <div>
+                <label className="block text-gray-700">Government ID</label>
+                <input
+                  type="file"
+                  className="w-full p-2 border rounded bg-white"
+                />
+              </div>
+              <div>
+                <label className="block text-gray-700">Preferred Role</label>
+                <select className="w-full p-2 border rounded focus:ring focus:ring-indigo-300">
+                  <option>Buyer</option>
+                  <option>Seller</option>
+                </select>
+              </div>
             </div>
-          </form>
-          <div className="text-xl flex justify-center text-white mt-4">
-            <p>Already have an account? </p>
-            <button
-              onClick={() => navigate("/")}
-              className="hover:underline font-medium ml-2"
-            >
-              Login
-            </button>
           </div>
+
+          {/* right */}
+          <div>
+            <h3 className="text-xl font-semibold text-gray-700 mb-3">
+              Business Details
+            </h3>
+            <div className="space-y-4">
+              <div>
+                <label className="block text-gray-700">Company Name</label>
+                <input
+                  type="text"
+                  className="w-full p-2 border rounded focus:ring focus:ring-indigo-300"
+                  placeholder="Your Company"
+                />
+              </div>
+              <div>
+                <label className="block text-gray-700">Business Sector</label>
+                <select
+                  className="w-full p-2 border rounded focus:ring focus:ring-indigo-300"
+                  value={businessSector}
+                  onChange={(e) => setBusinessSector(e.target.value)}
+                >
+                  <option value="">Select</option>
+                  <option value="Energy">Energy</option>
+                  <option value="Agriculture">Agriculture</option>
+                  <option value="Manufacturing">Manufacturing</option>
+                  <option value="Other">Other</option>
+                </select>
+              </div>
+
+              {businessSector === "Other" && (
+                <div>
+                  <label className="block text-gray-700">
+                    Specify Business Sector
+                  </label>
+                  <input
+                    type="text"
+                    className="w-full p-2 border rounded focus:ring focus:ring-indigo-300"
+                    placeholder="Enter your business sector"
+                    value={otherSector}
+                    onChange={(e) => setOtherSector(e.target.value)}
+                  />
+                </div>
+              )}
+
+              <div>
+                <label className="block text-gray-700">
+                  Company Registration Number
+                </label>
+                <input
+                  type="text"
+                  className="w-full p-2 border rounded focus:ring focus:ring-indigo-300"
+                  placeholder="123456789"
+                />
+              </div>
+              <div>
+                <label className="block text-gray-700">Country & Address</label>
+                <input
+                  type="text"
+                  className="w-full p-2 border rounded focus:ring focus:ring-indigo-300"
+                  placeholder="City, Country"
+                />
+              </div>
+              <div>
+                <label className="block text-gray-700">
+                  Goods and Services Tax Identification Number (GSTIN)
+                </label>
+                <input
+                  type="text"
+                  className="w-full p-2 border rounded focus:ring focus:ring-indigo-300"
+                  placeholder="GSTIN123456789"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-6">
+          <div className="flex items-center gap-2">
+            <input type="checkbox" className="w-4 h-4" />
+            <label className="text-gray-700">
+              I accept the{" "}
+              <a
+                href="#"
+                className="text-green-700 font-medium hover:underline"
+              >
+                Terms and Conditions
+              </a>
+            </label>
+          </div>
+        </div>
+        <div className="flex justify-center items-center mt-6">
+          <button
+            className="w-2/3 bg-green-600 text-white py-2 rounded hover:bg-green-700 transition"
+            onClick={() => navigate("/dashboard")}
+          >
+            Sign Up
+          </button>
+        </div>
+        <div className="text-center text-gray-700 mt-4">
+          Already have an account?{" "}
+          <button
+            onClick={() => navigate("/login")}
+            className="text-green-700 font-medium hover:underline"
+          >
+            Login
+          </button>
         </div>
       </div>
     </div>
