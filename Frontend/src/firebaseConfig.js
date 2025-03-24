565e1d6e82de1f68c378c2db334892a0656506d3
Rajdeep Mandal <mrajdeep651@gmail.com>
2025-03-24 21:33:34 +0530

    Firebase_initialized

---
+++ Frontend/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDT7Ya-4h4EeTopz_-Is5DDNNp4IxECByg",
+  authDomain: "carbon-credits-ccc29.firebaseapp.com",
+  projectId: "carbon-credits-ccc29",
+  storageBucket: "carbon-credits-ccc29.firebasestorage.app",
+  messagingSenderId: "955947920160",
+  appId: "1:955947920160:web:890de148f20591f18c4545",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
