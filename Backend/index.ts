0d324ad6fbb22ca187eab0e9095022a342fd7e01
Kumar Priyanshu <az.priyanshu.rockstar@gmail.com>
2025-03-27 00:26:17 +0530

    minor fix

--- Backend/index.ts
+++ Backend/index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3300;
+const port = process.env.PORT || 3200;
 
 import indexRoutes from "./src/routes/index.route";
 
@@ -18,4 +18,4 @@ app.use((req: Request, res: Response) => {
   res.status(200).json({ message: "Api Working..." });
 });
 
-app.listen(port, () => console.log("Server is running on port 3000"));
+app.listen(port, () => console.log("Server is running on port", port));
